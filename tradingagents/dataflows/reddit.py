733920b01267628b036b5f94d372e93ed83ab331
Rishabh Jain <rishabhsahan@gmail.com>
2026-05-16 23:54:22 +0530

    Initial Codebase

--- tradingagents/dataflows/reddit.py
+++ tradingagents/dataflows/reddit.py
@@ -28,7 +28,7 @@ _UA = "tradingagents/0.2 (+https://github.com/TauricResearch/TradingAgents)"
 # Default subreddits ordered roughly by signal density for ticker-specific
 # discussion. wallstreetbets has the most volume but most noise; stocks /
 # investing trend more measured. Caller can override.
-DEFAULT_SUBREDDITS = ("wallstreetbets", "stocks", "investing", "IndianStockMarket", "IndianStocks", "IndiaInvestments", "", "", "",)
+DEFAULT_SUBREDDITS = ("wallstreetbets", "stocks", "investing")
 
 
 def _fetch_subreddit(
